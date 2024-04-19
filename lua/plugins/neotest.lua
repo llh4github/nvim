0580c74305a0aa6ba30af3a534364b78bbf0fa69
llh4github <61351434+llh4github@users.noreply.github.com>
2024-04-19 18:24:16 +0800

    wip: ？
    成了，改了一下又无效了

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-go",
+    },
+    opts = {
+      adapters = {
+        ["neotest-go"] = {
+          -- Here we can set options for neotest-go, e.g.
+          -- args = { "-tags=integration" },
+          recursive_run = true,
+        },
+      },
+    },
+  },
+}
