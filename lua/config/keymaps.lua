5064fbeb362f137a7e0a0da8cff75fad03a31d24
llh <lilinhong_coding@foxmail.com>
2024-05-18 15:02:24 +0800

    feat: 快捷键设置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+-- 删除快捷键 ---
+unmap("n", "<leader>l", { desc = "Lazy" })
+unmap("n", "<c-/>", { desc = "terminal" })
+unmap("n", "<leader>fe", { desc = "Explorer NeoTree (Root Dir)" })
+
+-- 删除快捷键 end ---
+
+-- 自定义快捷键映射 ---
+map("n", "<a-1>", function()
+  require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+end, { desc = "Explorer NeoTree (Root Dir)" })
+-- 自定义快捷键映射 end ---
