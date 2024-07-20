e00b2cc2c70eb5fbc9d44b72316f74d7248969f4
llh <lilinhong_coding@foxmail.com>
2024-07-20 14:40:13 +0800

    feat: 增加快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,18 +4,18 @@
 local map = vim.keymap.set
 local unmap = vim.keymap.del
 
-
 -- 自定义快捷键映射 ---
 map("n", "<a-1>", function()
   require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
 end, { desc = "Explorer NeoTree (Root Dir)" })
+
+map("n", "<leader>xc", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 -- 自定义快捷键映射 end ---
-
-
 
 -- 删除快捷键 ---
 unmap("n", "<leader>l", { desc = "Lazy" })
 unmap("n", "<c-/>", { desc = "terminal" })
+unmap("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 unmap("n", "<leader>fe", { desc = "Explorer NeoTree (Root Dir)" })
 -- 去掉 <leader><leader> 来搜索文件的映射
 unmap("n", "<leader><space>", { desc = "Find Files (root dir)" })
