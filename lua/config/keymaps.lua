87db70c1ce328ab4f44f6233262fe195d80b3fdb
llh <lilinhong_coding@foxmail.com>
2024-07-20 14:09:36 +0800

    feat: 通用配置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+
+-- 自定义快捷键映射 ---
+map("n", "<a-1>", function()
+  require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+end, { desc = "Explorer NeoTree (Root Dir)" })
+-- 自定义快捷键映射 end ---
+
+
+
+-- 删除快捷键 ---
+unmap("n", "<leader>l", { desc = "Lazy" })
+unmap("n", "<c-/>", { desc = "terminal" })
+unmap("n", "<leader>fe", { desc = "Explorer NeoTree (Root Dir)" })
+-- 去掉 <leader><leader> 来搜索文件的映射
+unmap("n", "<leader><space>", { desc = "Find Files (root dir)" })
+-- 删除快捷键 end ---
