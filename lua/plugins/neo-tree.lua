3cdc44beb72bf627a77ac1fe1b6cb78832d70006
llh <lilinhong_coding@foxmail.com>
2024-11-30 11:21:16 +0800

    init project

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      width = 25,
+    },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+    },
+  },
+}
