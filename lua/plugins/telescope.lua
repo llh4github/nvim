dbf870be0ee12558b1cc6222e1369835f8854bd2
lilinh <sj-lilinh@dimpt.com>
2024-12-03 10:30:30 +0800

    fix: 修改搜索配置
    忽略特定文件夹

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaluts = {
+      file_ignore_patterns = {
+        "node_modules",
+        ".git",
+        "dist",
+      },
+    },
+  },
+}
