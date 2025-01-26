dbc48767e51693c3ed1ffe1530750c2295e6988a
llh4github <lilinhong_coding@foxmail.com>
2025-01-26 15:03:37 +0800

    feat: 添加VSCode键映射支持，增强编辑器导航和操作功能

---
+++ lua/myvscode/keymaps.lua
@@ -0,0 +1,139 @@
+local map = vim.keymap.set
+local vscode = require('vscode')
+
+map(
+    "n", "H",
+    function()
+        vscode.action('workbench.action.previousEditor')
+    end,
+    { desc = "previousEditor", noremap = true }
+)
+map(
+    "n", "L",
+    function()
+        vscode.action('workbench.action.nextEditor')
+    end,
+    { desc = "nextEditor", noremap = true }
+)
+map(
+    "n", "]e",
+    function()
+        vscode.action('editor.action.marker.next')
+    end,
+    { desc = "goTo next marker" }
+)
+map(
+    "n", "[e",
+    function()
+        vscode.action('editor.action.marker.prev')
+    end,
+    { desc = "goTo prev marker" }
+)
+
+map(
+    "n", "<leader>aa",
+    function()
+        vscode.action('workbench.panel.chat')
+    end,
+    { desc = "ai chat panel" }
+)
+map(
+    "n", "<leader>bo",
+    function()
+        vscode.action('workbench.action.closeOtherEditors')
+    end,
+    { desc = "closeOtherEditors" }
+)
+map(
+    "n", "<leader>bd",
+    function()
+        vscode.action('workbench.action.files.save')
+        vscode.action('workbench.action.closeActiveEditor')
+    end,
+    { desc = "save files and close editor" }
+)
+
+map(
+    "i", "<C-a>",
+    function()
+        vscode.action('editor.action.selectAll')
+    end,
+    { desc = "selectAll in insert mode", noremap = true }
+)
+
+map(
+    { "n", "v" }, "<leader>ca",
+    function()
+        vscode.action('editor.action.sourceAction')
+    end,
+    { desc = "sourceAction" }
+)
+map(
+    { "n", "v" }, "<leader>cr",
+    function()
+        vscode.action('editor.action.rename')
+    end,
+    { desc = "rename" }
+)
+
+map(
+    { "n", "v" }, "<leader>cr",
+    function()
+        vscode.action('editor.action.rename')
+    end,
+    { desc = "rename" }
+)
+map(
+    "n", "<leader>cf",
+    function()
+        vscode.action('editor.action.formatDocument')
+        vscode.action('workbench.action.files.save')
+    end,
+    { desc = "format Document and Save files" }
+)
+map(
+    "n", "<leader>fr",
+    function()
+        vscode.action('workbench.action.openRecent')
+    end,
+    { desc = "openRecent files" }
+)
+
+map(
+    "n", "<leader>lZ",
+    function()
+        vscode.action('workbench.action.toggleZenMode')
+    end,
+    { desc = "toggleZenMode" }
+)
+map(
+    "n", "<leader>lF",
+    function()
+        vscode.action('workbench.action.toggleFullScreen')
+    end,
+    { desc = "toggleFullScreen" }
+)
+
+map(
+    "n", "<leader>gi",
+    function()
+        vscode.action('editor.action.goToImplementation')
+    end,
+    { desc = "goToImplementation" }
+)
+
+map(
+    "n", "<leader>gy",
+    function()
+        vscode.action('editor.action.goToTypeDefinition')
+    end,
+    { desc = "goToImplementation" }
+)
+
+map(
+    "n", "<leader>wm",
+    function()
+        vscode.action('workbench.action.minimizeOtherEditors')
+    end,
+    { desc = "minimizeOtherEditors" }
+)
