3958c49590ea1b82a3f5b1a74136bef91a7d1979
llh4github <lilinhong_coding@foxmail.com>
2025-04-01 10:06:43 +0800

    feat: 增加Windows系统支持，设置PowerShell为默认shell

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+-- 检测是否为 Windows 系统
+local is_windows = package.config:sub(1, 1) == '\\' or os.getenv('OS') == 'Windows_NT'
+if is_windows then
+    vim.o.shell = "pwsh.exe"
+    vim.o.shellcmdflag = "-NoLogo -NoProfile -Command Set-Location -LiteralPath '%s'"
+end
 require("config.lazy")
