753927fe221c4351defd1697001dabff2176aac1
llh4github <lilinhong_coding@foxmail.com>
2025-12-31 00:09:17 +0800

    主题配置

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    -- style = "night",
+    style = "moon",
+    transparent = true,
+    styles = {
+      sidebars = "transparent", -- 侧边栏透明
+      floats = "transparent", -- 浮动窗口透明
+    },
+  },
+}
