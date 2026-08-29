791a2902f1e7318aeb4af76b350396c30eaa8680
llh4github <lilinhong_coding@foxmail.com>
2026-08-29 16:01:30 +0800

    fix: define omp as full custom ACP adapter

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -12,11 +12,50 @@ return {
     },
     adapters = {
       acp = {
-        omp = {
-          type = "custom",
-          command = "omp",
-          args = { "acp" },
-        },
+        omp = function()
+          local helpers = require("codecompanion.adapters.acp.helpers")
+          return {
+            name = "omp",
+            formatted_name = "Oh My Pi",
+            type = "acp",
+            roles = {
+              llm = "assistant",
+              user = "user",
+            },
+            commands = {
+              default = {
+                "omp",
+                "acp",
+              },
+            },
+            defaults = {
+              mcpServers = {},
+              timeout = 20000,
+            },
+            parameters = {
+              protocolVersion = 1,
+              clientCapabilities = {
+                fs = { readTextFile = true, writeTextFile = true },
+              },
+              clientInfo = {
+                name = "CodeCompanion.nvim",
+                version = "1.0.0",
+              },
+            },
+            handlers = {
+              setup = function(self)
+                return true
+              end,
+              auth = function(self)
+                return true
+              end,
+              form_messages = function(self, messages, capabilities)
+                return helpers.form_messages(self, messages, capabilities)
+              end,
+              on_exit = function(self, code) end,
+            },
+          }
+        end,
       },
     },
     interactions = {
